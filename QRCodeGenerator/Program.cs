76ed5da9a7459dc1ac40b1a8d32655e5f8cdffcc
Tyler Barnett <tyler.barnett@me.com>
2019-06-16 22:51:49 -0600

    Init

--- QRCodeGenerator/Program.cs
+++ QRCodeGenerator/Program.cs
@@ -1,4 +1,12 @@
 ﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using NDesk.Options;
+using System.Reflection;
+using QRCoder;
+using System.Text;
+//using System.Windows.Markup;
 
 namespace QRCodeGenerator
 {
@@ -6,7 +14,22 @@ namespace QRCodeGenerator
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      QRCoder.QRCodeGenerator qrGenerator = new QRCoder.QRCodeGenerator();
+
+      QRCodeData qrCodeData = qrGenerator.CreateQrCode("TESTING THIS CODE BLAB BLABE.", QRCoder.QRCodeGenerator.ECCLevel.Q);
+      QRCode qrCode = new QRCode(qrCodeData);
+      Bitmap qrCodeImage = qrCode.GetGraphic(20);
+      var directory = (Path.Combine(Environment.CurrentDirectory, "../", "TempSiteDocuments"));
+      if (!Directory.Exists(directory))
+      {
+        Directory.CreateDirectory(directory);
+      }
+      var filePath = Path.Combine(directory, "Test");
+      using (var stream = new FileStream(filePath, FileMode.Create))
+      {
+        qrCodeImage.Save(stream, ImageFormat.Jpeg);
+        //qrCodeImage.CopyTo(stream);
+      }
     }
   }
 }
